4e4d20c971fd7b56bbe4fe33aeb5440c397a107c
Julia Hadja <juliahadja@gmail.com>
2025-06-06 14:59:25 -0300

    fix textos

--- src/Routers/Home.jsx
+++ src/Routers/Home.jsx
@@ -4,10 +4,9 @@ import Onda from '../assets/icons/onda.png'
 import './Styles.css'
 
 export default function Home() {
-    
     return (
         <div className='bg-[url(./assets/mobile.png)]  md:bg-[url(./assets/background.png)] bg-cover bg-no-repeat pt-40  '>
-            <div className='p-10 pb-80 md:pb-80 md:pl-50'>
+            <div className='p-10 pb-80 md:pb-100 md:pl-50'>
                 <h1 className=" text-4xl md:text-5xl text-cyan-50">HYDROGUARD</h1>
                 <p className="md:text-2xl text-cyan-400 font-light">Quando a água sobe, a tecnologia se levanta.</p>
             </div>
@@ -15,18 +14,17 @@ export default function Home() {
                 <div className='bg-gray-950 text-center p-4 rounded-md lg:p-10'>
                     <img className='w-20 mx-auto' src={Agua} alt="icone de água" />
                     <h3 className='text-cyan-50 text-2xl p-2 pb-5'>“Barragem doméstica”</h3>
-                    <p className='text-cyan-50 text-sm md:text-lg'>HydroGuard é um sistema de defesa automática contra alagamentos.
-                        Ele funciona como uma espécie de "barragem doméstica", capaz de se erguer sozinha ao detectar risco.</p>
+                    <p className='text-cyan-50 text-sm md:text-lg'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Natus quam vitae inventore aperiam saepe quod, hic, animi mollitia sunt est unde doloribus, ipsam iste aspernatur facere fugiat nostrum provident nesciunt.</p>
                 </div>
                 <div className='bg-gray-950 text-center p-4 rounded-md lg:p-10'>
                     <img className='w-20 mx-auto' src={Gota} alt="icone de Gota" />
                     <h3 className='text-cyan-50 text-2xl p-2 pb-5'>Sensores de nível</h3>
-                    <p className='text-cyan-50 text-sm md:text-lg'>Com sensores de nível d’água — que podem ser instalados na calçada, quintal ou porta —  o    sistema monitora continuamente o aumento da água.</p>
+                    <p className='text-cyan-50 text-sm md:text-lg'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Natus quam vitae inventore aperiam saepe quod, hic, animi mollitia sunt est unde doloribus, ipsam iste aspernatur facere fugiat nostrum provident nesciunt.</p>
                 </div>
                 <div className='bg-gray-950 text-center p-6 rounded-md lg:p-10'>
                     <img className='w-20 mx-auto' src={Onda} alt="icone de onda" />
                     <h3 className='text-cyan-50 text-2xl p-2 pb-5'>Sistema anti-enchente</h3>
-                    <p className='text-cyan-50 text-sm md:text-lg'>É como ter um sistema de proteção anti-enchente na sua porta, pronto para agir.</p>
+                    <p className='text-cyan-50 text-sm md:text-lg'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Natus quam vitae inventore aperiam saepe quod, hic, animi mollitia sunt est unde doloribus, ipsam iste aspernatur facere fugiat nostrum provident nesciunt.</p>
                 </div>
             </div>
         </div>
